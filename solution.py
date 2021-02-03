3afc534fa893d3f3463886d95b82dbe5daafbbf2
chatterbot66 <70983339+chatterbot66@users.noreply.github.com>
2021-02-02 21:50:23 -0500

    Add files via upload

--- solution.py
+++ solution.py
@@ -23,10 +23,11 @@ def welcome_assignment_answers(question):
         answer = "No"
     elif question == "What layer from the TCP/IP model the protocol DHCP belongs to?": # - The answer should be a numeric number"
         answer = 5
-    else question == "What layer of the TCP/IP model the protocol TCP belongs to?":  # - The answer should be a numeric number"
+    elif question == "What layer of the TCP/IP model the protocol TCP belongs to?":  # - The answer should be a numeric number"
         answer = 4
-    
-    return(answer)
+    else:
+        
+        return()
 
 # Complete all the questions.
 
