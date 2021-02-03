1095d0773a8b51b3449cadeb8016b515f3d7b1dd
chatterbot66 <70983339+chatterbot66@users.noreply.github.com>
2021-02-02 21:31:54 -0500

    Add files via upload

---
+++ solution.py
@@ -0,0 +1,40 @@
+
+### welcome_assignment_answers
+### Input - All eight questions given in the assignment.
+### Output - The right answer for the specific question.
+
+
+
+def welcome_assignment_answers(question):
+    
+    #Another way to implement is using a "case" statements similar to C.
+    
+    if question == "Are encoding and encryption the same? - Yes/No":
+        answer = "No"
+    elif question == "Is it possible to decrypt a message without a key? - Yes/No":
+        answer = "No"
+    elif question == "Is it possible to decode a message without a key? - Yes/No":
+        answer = "Yes"
+    elif question == "Is a hashed message supposed to be un-hashed? - Yes/No":
+        answer = "No"
+    elif question == "What is the MD5 hashing value to the following message: 'NYU Computer Networking' - ": 
+        answer = "42b76fe51778764973077a5a94056724"
+    elif question == "Is MD5 a secured hashing algorithm? - Yes/No":
+        answer = "No"
+    elif question == "What layer from the TCP/IP model the protocol DHCP belongs to?": # - The answer should be a numeric number"
+        answer = 5
+    else question == "What layer of the TCP/IP model the protocol TCP belongs to?":  # - The answer should be a numeric number"
+        answer = 4
+    
+    return(answer)
+
+# Complete all the questions.
+
+
+if __name__ == "__main__":
+
+
+    #use this space to debug and verify that the program works
+
+    debug_question = "Are encoding and encryption the same? - Yes/No"
+    (welcome_assignment_answers(debug_question))
